5c354b0d283a03bf1ea388346c07bd04ca4230d1
Sebastian Warnke <sw-gitea@sebwarnke.com>
2019-04-23 15:13:41 +0200

    add solution to lab 7

--- src/test/java/com/camunda/training/ProcessTests.java
+++ src/test/java/com/camunda/training/ProcessTests.java
@@ -1,13 +1,18 @@
 package com.camunda.training;
 
-import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.*;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.init;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.execute;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.jobQuery;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.taskService;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.camunda.bpm.engine.runtime.Job;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
@@ -19,7 +24,7 @@ import org.junit.Test;
 public class ProcessTests {
 
   private static final String KEY_OF_PROCESS_DEFINITION_UNDER_TEST = "TweetApprovalProcess";
-  
+
   @Rule
   public ProcessEngineRule rule = new ProcessEngineRule();
 
@@ -29,30 +34,33 @@ public class ProcessTests {
   }
 
   @Test
-  @Deployment(resources="tweet-approval.bpmn")
+  @Deployment(resources = "tweet-approval.bpmn")
   public void testHappyPath() {
 
     Map<String, Object> variables = new HashMap<String, Object>();
     variables.put("approved", true);
-    variables.put("content", "Exercise 4 test - Sebastian");
-    
+    variables.put("content", "Exercise 7 test - Sebastian");
+
     ProcessInstance processInstance = runtimeService().startProcessInstanceByKey(KEY_OF_PROCESS_DEFINITION_UNDER_TEST, variables);
 
     assertThat(processInstance).isWaitingAt("UserTask_ReviewTweet");
-    
-    List<Task> taskList = taskService().createTaskQuery()
-        .taskCandidateGroup("management")
-        .processInstanceId(processInstance.getId())
-        .list();
-    
+
+    List<Task> taskList = taskService().createTaskQuery().taskCandidateGroup("management").processInstanceId(processInstance.getId()).list();
+
     assertThat(taskList).isNotNull();
     assertThat(taskList).hasSize(1);
-    
+
     Task task = taskList.get(0);
-    
+
     Map<String, Object> approvedMap = new HashMap<String, Object>();
     approvedMap.put("approved", true);
     taskService().complete(task.getId(), approvedMap);
+
+    List<Job> jobList = jobQuery().processInstanceId(processInstance.getId()).list();
+    assertThat(jobList).hasSize(1);
+    Job job = jobList.get(0);
+    execute(job);
     
+    assertThat(processInstance).isEnded();
   }
 }
