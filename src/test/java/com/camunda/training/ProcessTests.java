2862475b8812e7b389cf91b9aa5ab0ae9e106edf
Sebastian Warnke <sw-gitea@sebwarnke.com>
2019-04-23 14:48:35 +0200

    add lab 5 solution

--- src/test/java/com/camunda/training/ProcessTests.java
+++ src/test/java/com/camunda/training/ProcessTests.java
@@ -1,13 +1,15 @@
 package com.camunda.training;
 
-import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.init;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
-import static org.camunda.bpm.engine.test.assertions.cmmn.CmmnAwareTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.*;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.junit.Before;
@@ -29,14 +31,28 @@ public class ProcessTests {
   @Test
   @Deployment(resources="tweet-approval.bpmn")
   public void testHappyPath() {
-    // Create a HashMap to put in variables for the process instance
+
     Map<String, Object> variables = new HashMap<String, Object>();
     variables.put("approved", true);
     variables.put("content", "Exercise 4 test - Sebastian");
     
-    // Start process with Java API and variables
     ProcessInstance processInstance = runtimeService().startProcessInstanceByKey(KEY_OF_PROCESS_DEFINITION_UNDER_TEST, variables);
-    // Make assertions on the process instance
-    assertThat(processInstance).isEnded();
+
+    assertThat(processInstance).isWaitingAt("UserTask_ReviewTweet");
+    
+    List<Task> taskList = taskService().createTaskQuery()
+        .taskCandidateGroup("management")
+        .processInstanceId(processInstance.getId())
+        .list();
+    
+    assertThat(taskList).isNotNull();
+    assertThat(taskList).hasSize(1);
+    
+    Task task = taskList.get(0);
+    
+    Map<String, Object> approvedMap = new HashMap<String, Object>();
+    approvedMap.put("approved", true);
+    taskService().complete(task.getId(), approvedMap);
+    
   }
 }
