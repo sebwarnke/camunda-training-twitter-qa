c4060ff8694b758f43944638298567b8136fbbec
Sebastian Warnke <sw-gitea@sebwarnke.com>
2019-04-24 23:44:57 +0200

    test(process): implemented proper mocking

--- src/test/java/com/camunda/training/ProcessTests.java
+++ src/test/java/com/camunda/training/ProcessTests.java
@@ -7,6 +7,10 @@ import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.execute
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.jobQuery;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.runtimeService;
 import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.taskService;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.job;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,10 +21,20 @@ import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.mock.Mocks;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import twitter4j.Status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProcessTests {
 
   private static final String KEY_OF_PROCESS_DEFINITION_UNDER_TEST = "TweetApprovalProcess";
@@ -28,15 +42,24 @@ public class ProcessTests {
   @Rule
   public ProcessEngineRule rule = new ProcessEngineRule();
 
+  @Mock
+  private TwitterService twitterService;
+  
   @Before
   public void setup() {
     init(rule.getProcessEngine());
+
+    MockitoAnnotations.initMocks(this);
+    Mocks.register("CreateTweetDelegate", new CreateTweetDelegate(twitterService));
   }
 
+  
   @Test
   @Deployment(resources = "tweet-approval.bpmn")
-  public void testHappyPath() {
+  public void testHappyPath() throws Exception{
 
+    when(twitterService.updateStatus(any(String.class))).thenReturn(mock(Status.class));
+    
     Map<String, Object> variables = new HashMap<String, Object>();
     variables.put("approved", true);
     variables.put("content", "Exercise 7 test - Sebastian");
@@ -62,5 +85,22 @@ public class ProcessTests {
     execute(job);
     
     assertThat(processInstance).isEnded();
+  }
+  
+  @Test(expected = Exception.class)
+  @Deployment(resources = "tweet-approval.bpmn")
+  public void testTwice() throws Exception {
+
+    when(twitterService.updateStatus(any(String.class))).thenReturn(mock(Status.class)).thenThrow(new Exception());
+    
+    ProcessInstance processInstance1 = runtimeService().createProcessInstanceByKey(KEY_OF_PROCESS_DEFINITION_UNDER_TEST).setVariable("content", "random content").startBeforeActivity("Task_0lfmlyu").execute();
+    assertThat(processInstance1).isWaitingAt("Task_0lfmlyu");
+    execute(job());
+    ProcessInstance processInstance2 = runtimeService().createProcessInstanceByKey(KEY_OF_PROCESS_DEFINITION_UNDER_TEST).setVariable("content", "random content").startBeforeActivity("Task_0lfmlyu").execute();
+    assertThat(processInstance2).isWaitingAt("Task_0lfmlyu");
+    execute(job());
+    
+    assertThat(processInstance1).isNotNull();
+    
   }
 }
